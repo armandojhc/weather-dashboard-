d2636dc1207846cba607d8691fc76300194cb6b7
Armando Herrera Canelon <armando.herrera91@gmail.com>
2020-01-17 19:31:42 -0500

    Added weather five day forecast cards

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 var pastSearchCities = [];
 
-
+// var months_arr = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
 
 $(document).ready(function () {
 
@@ -11,6 +11,8 @@ $(document).ready(function () {
 
     
         loadPreviousSearch () /*Should include search history on the left and previous city search with 5 day forecast*/
+
+    
 });
 
 
@@ -29,8 +31,10 @@ function loadPreviousSearch() {
     }
 
     
+    // todayWeather();
 
     populatePastSearchCities();
+
 
 }
 
@@ -83,7 +87,20 @@ function getWeather (city) {
 
 function todayWeather (weather) {
     console.log(weather);
-    $('#city-name').text(weather.name);
+    // $('#city-name').text(weather.name);
+
+    // Date Converter from Unix to Date
+
+    var todayDate = weather.dt;
+
+    // console.log(todayDate);
+
+    var date = new Date(todayDate*1000);
+
+    // console.log(date);
+
+    $('#city-name').text(weather.name + " : " + " " + date);
+
     var temp = Math.floor(((weather.main.temp - 273.15) * 9/5) + 32);
     
     $('#city-temperature').text(`Temperature: ${temp} F`);
@@ -93,8 +110,14 @@ function todayWeather (weather) {
     var windSpeed = ((weather.wind.speed) * 2.237).toFixed(2);  
     $('#city-windspeed').text(`Wind Speed: ${windSpeed} MPH`);
 
-    //Now get the UV index for the city (weather.name)
-    var queryUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + weather.name + '&appid=a53029242518ad3a567bbcab79de12cc';
+
+    var cityLat = weather.coord.lat;
+    var cityLon =weather.coord.lon;
+
+    console.log(cityLat);
+    console.log(cityLon);
+    
+    var queryUrl = 'https://api.openweathermap.org/data/2.5/uvi?appid=a53029242518ad3a567bbcab79de12cc&lat=' + cityLat + '&lon=' + cityLon;
 
     $.ajax({
         url: queryUrl,
@@ -103,65 +126,8 @@ function todayWeather (weather) {
 
         console.log(response);
 
-
-        //Get the UV index from the response object
-        //Add the UV index like $('#city-temperature').text(`Temperature: ${temp} F`);
-        
+        $('#city-uv').text(`UV Index: ${response.value}`);
         
     });
 }
 
-
-
-
-// Testing if my api call works.
-
-
-
-
-// function weatherSearch () {
-//     var city = $('.form-control').text();
-//     city.val(pastSearchCities)
-//     input
-// })
-
-// This is where I create function that allows me add the current weather to the main card. 
-
-// var city = "Caracas";
-
-// function addCurrentWeather (city) {
-//     var queryUrl = 'https://api.openweathermap.org/data/2.5/forecast?appid=a53029242518ad3a567bbcab79de12cc&q=' + city;
-
-//     $.ajax({
-//         queryUrl: URL,
-//         method: "GET"
-//     }).then( function (response) {
-
-//         console.log(response);
-
-//         var currentCityWeatherTitle= $("card-title")
-//         currentCityWeatherTitle.text(response.q);
-
-
-//         $('.card-body').append(currentCityWeatherTitle);
-
-
-
-//     });
-// }
-
-
-
-
-
-
-// Trying to set the searched cities and sending them to local storage
-
-// function searchCityButton () {
-//     var searchCities = $('.form-control').val();
-    
-//     pastSearchCities = searchCities
-
-
-//     localStorage.setItem("search" , JSON.stringify(pastSearchCities));
-// }
